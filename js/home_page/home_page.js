e031d36a6c971b18c92d26e4394a23084d118d2f
hungnd.nuce94@gmail.com <hungnd.nuce94@gmail.com>
2023-05-25 17:53:40 +0700

    update_news_page

---
+++ js/home_page/home_page.js
@@ -0,0 +1,15 @@
+// let bar = document.querySelector('.mini_bar');
+// let menu = document.querySelector('.main_menu')
+
+// bar.addEventListener('click', () => {
+//     menu.classList.toggle('active');
+// })
+
+function handle_mobile_nav() {
+    let btn = document.querySelector('.mini_bar'),
+        nav = document.querySelector('.main_menu ul');
+    btn.addEventListener('click', function() {
+        nav.classList.toggle('show');
+    });
+}
+handle_mobile_nav();
